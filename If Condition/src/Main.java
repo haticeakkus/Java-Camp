c86986e4e22fc9ddc20983db62cdd3cd76d98e13
haticeakkus <80623945+haticeakkus@users.noreply.github.com>
2022-10-23 16:44:56 +0300

    if condition is sampled

---
+++ If Condition/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int number = 25;
+		if(number>20)
+			System.out.println(number + " is greather than 20.");
+		else if(number<20)
+			System.out.println(number + " is less than 20.");
+		else
+			System.out.println(number + " equals 20");
+	}
+
+}
