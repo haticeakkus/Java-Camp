060bff88fb8d5cdadcf659cedfe560b28346c4fe
haticeakkus <80623945+haticeakkus@users.noreply.github.com>
2022-10-23 16:42:33 +0300

    variables are declared

---
+++ Variables/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+//reusability
+	public static void main(String[] args) {
+		String studentInfo = "Öğrenci sayısı: " ;
+		int studentNumber = 20;
+		
+		System.out.println(studentInfo + studentNumber);
+	}
+
+}
