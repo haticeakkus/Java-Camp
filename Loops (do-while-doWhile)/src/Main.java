105e749550597662c22980107bcfc26bc0d2d078
haticeakkus <80623945+haticeakkus@users.noreply.github.com>
2022-10-23 16:46:21 +0300

    all loop types are sampled

---
+++ Loops (do-while-doWhile)/src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// For Loop
+		for(int i=1 ; i<=10 ; i++) {
+			System.out.println(i);
+		}
+		System.out.println("The for loop is finished!");
+		
+		
+		// While Loop
+		int j = 1;
+		while(j<=10) {
+			System.out.println(j);
+			j+=2;
+		}
+		System.out.println("The while loop is finished!");
+		
+		// doWhile Loop
+		int k=2;
+		do {
+			System.out.println(k);
+		 k+=2;
+		}while(k<=10);
+		System.out.println("The do-while loop is finished!");
+	}
+
+}
