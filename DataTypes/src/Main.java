fbdbe1176842bb9ef7aa92253869d017160ece0f
haticeakkus <80623945+haticeakkus@users.noreply.github.com>
2022-10-23 16:45:48 +0300

    data types are explained

---
+++ DataTypes/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//byte =>  1 byte -> Stores whole numbers from -128 to 127
+		//short	=> 2 bytes -> Stores whole numbers from -32,768 to 32,767
+		int num = 5;				// 4 bytes	=> Stores whole numbers from -2,147,483,648 to 2,147,483,647
+		//long =>  8 bytes -> Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+		
+		float floatNum = 5.99f;		// 	4 bytes	=> Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+		//double => 8 bytes -> Stores fractional numbers. Sufficient for storing 15 decimal digits
+		
+		boolean	bool = true;		// 1 bit => Stores true or false values
+		
+		char letter = 'A';			// 2 bytes	=> Stores a single character/letter or ASCII values
+		
+		String text = "Hello";
+		
+	}
+
+}
