6acfedddf69a6106b93fb86e5f12c5e4be8edb43
haticeakkus <80623945+haticeakkus@users.noreply.github.com>
2022-10-23 17:18:56 +0300

    understand the classes

---
+++ Classes/src/CustomerManager.java
@@ -0,0 +1,11 @@
+
+public class CustomerManager {
+	
+	public void Add() {
+		System.out.println("Customer added");
+	}
+	public void Delete() {
+		System.out.println("Customer deleted");
+	}
+	
+}
---
+++ Classes/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//reference type
+		CustomerManager customerManager = new CustomerManager();
+		//Stack created for Customer Manager
+		
+		customerManager.Add();
+		customerManager.Delete();
+	}
+
+}
