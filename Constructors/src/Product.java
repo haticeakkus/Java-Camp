0aa77a7010284be8047d5552971cd1e671a754a2
haticeakkus <80623945+haticeakkus@users.noreply.github.com>
2022-10-25 22:00:41 +0300

    understand how to use constructors

---
+++ Constructors/src/Product.java
@@ -0,0 +1,72 @@
+public class Product {
+	
+	// constructor
+	public Product() {
+		System.out.println("Constructor blok worked.");
+	}
+	
+	//overloading : same name different parameter
+	public Product(int id, String name, String description, double price, int stockAmount, String code) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.stockAmount = stockAmount;
+		this.code = code;
+	}
+
+	// valid only in the block in which it is defined
+	// attribute | field
+	private int id;
+	private String name;
+	private String description;
+	private double price;
+	private int stockAmount;
+	private String code;
+
+	// getter : read only
+	public int getId() {
+		return id;
+	}
+
+	// setter : change value
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getStockAmount() {
+		return stockAmount;
+	}
+
+	public void setStockAmount(int stockAmount) {
+		this.stockAmount = stockAmount;
+	}
+
+	public String getCode() {
+		return this.name.substring(0, 1) + id;
+	}
+}
